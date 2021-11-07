e2c1ffac67ca4597c78ae1ee2628559e49139bbd
Misha Shmidt <mishashmidt0@gmail.com>
2021-11-07 19:07:41 +0300

    fix: task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,11 @@
 
-//* Синхронные функции не использовал ! 
 const fs = require('fs');
-fs.readFile(`${__dirname}/text.txt`, "utf8", (error, data) => {
-    console.log(data)
-})
+const path = require('path/posix');
+const { stdout } = require('process');
+
+const stream = fs.createReadStream(
+    path.join(__dirname, '/text.txt'),
+    'utf8'
+);
+stream.on('data', (data) => stdout.write(data));
+stream.on('error', (err) => stdout.write(`Err: ${err}`));
